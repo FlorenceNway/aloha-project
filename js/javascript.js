09f732d9c8ec65cbc4c9a11c87990e65115c4108
Florence Nway <florence.nway@hotmail.com>
2018-04-23 15:19:37 +0100

    final update

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,11 @@
 $('.main-carousel').flickity({
   cellAlign: 'left',
   contain: true,
+  groupCells:true,
   wrapAround: true
   
 });
 
+$("input[type='submit']").click(function(){
+	alert("Thanks for subscribing!");
+});
